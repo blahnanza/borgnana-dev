b6dfef06c0dd851dbbe3ed61417cb88ce08c8953
tristan <git@blahnana.com>
2016-06-29 16:38:36 +1000

    changes so that pauly_ff doesn't complain about red in the vagrant provisioning, even though it's really a combination bug between the ubuntu vagrant box and the privileged vagrant shell provisioning method

--- provisioning/install_ansible.sh
+++ provisioning/install_ansible.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
-DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
-sudo apt-get install -y build-essential
-sudo apt-get install -y libffi-dev
-#sudo apt-get install -y ansible
-sudo apt-get install -y python-pip python-dev
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
+#!/bin/bash
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libffi-dev
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python-pip python-dev
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common
+
+sudo DEBIAN_FRONTEND=noninteractive apt-add-repository -y ppa:ansible/ansible 2>&1
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ansible
