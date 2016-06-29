8b3c4f9df946fec58e388c626747351c7e56b35d
tristan <git@blahnana.com>
2016-06-29 13:56:21 +1000

    updated to reduce errors

--- provisioning/install_ansible.sh
+++ provisioning/install_ansible.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-export DEBIAN_FRONTEND=noninteractive
+DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get install -y build-essential
 sudo apt-get install -y libffi-dev
 #sudo apt-get install -y ansible
 sudo apt-get install -y python-pip python-dev
+sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
 sudo apt-get install -y ansible
